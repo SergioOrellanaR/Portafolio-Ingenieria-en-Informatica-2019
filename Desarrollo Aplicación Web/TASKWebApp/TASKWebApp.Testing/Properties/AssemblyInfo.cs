613f1defb00737abaf1f77a287f0e31bebe1a260
Sergio Orellana Rey <orellana123@gmail.com>
2019-09-05 09:21:48 -0400

    Se agrega arquitectura de proyecto web
    -Se agrega arquitectura de proyecto web, este consta de 5 capas:
    
    -Data : Capa de datos
    -Business: Capa de negocio y procesos asociados a estos
    -View: Capa vista (Aplicación web, proyecto de inicio por default)
    -Console: Capa con consola de comandos para hacer pruebas rapidas y cortas
    -Testing: Capa de pruebas unitarias para pruebas importantes y flujos.

---
+++ Desarrollo Aplicación Web/TASKWebApp/TASKWebApp.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TASKWebApp.Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TASKWebApp.Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d67ea12-71c3-4bca-ae10-c8d685d19a04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
