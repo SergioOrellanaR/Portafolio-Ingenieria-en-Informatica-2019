247acb0e53b84ee868828b454e0369e50ded4b5d
Sergio Orellana <orellana123@gmail.com>
2019-09-17 22:00:20 -0300

    Se añaden test + los ComboboxLoader principales
    Se agrega testing, y diccionarios para traer listas de información read only. Falta añadir el de las tareas (marcado como "To Do") el cual se agregará al crear su clase correspondiente

--- Desarrollo Aplicación Web/TASKWebApp/TASKWebApp.Business/Helpers/ComboBoxDataLoader.cs
+++ Desarrollo Aplicación Web/TASKWebApp/TASKWebApp.Business/Helpers/ComboBoxDataLoader.cs
@@ -6,6 +6,65 @@ namespace TASKWebApp.Business.Helpers
     public class ComboBoxDataLoader
     {
         private static Dictionary<int, string> _gender;
+        private static Dictionary<int, string> _region; 
+        private static Dictionary<int, string> _dayOfWeek;
+        private static Dictionary<int, string> _month;
+        private static Dictionary<int, string> _taskStatus;
+
+        public static Dictionary<int, string> TaskStatus
+        {
+            get
+            {
+                _taskStatus = new Dictionary<int, string>();
+                foreach (Data.TASK_STATUS taskStatus in Connection.ProcessSA_DB.TASK_STATUS.ToList())
+                {
+                    _taskStatus.Add((int)taskStatus.ID, taskStatus.STATUS);
+                }
+                return _taskStatus;
+            }
+        }
+
+
+        public static Dictionary<int, string> Month
+        {
+            get
+            {
+                _month = new Dictionary<int, string>();
+                foreach (Data.MONTH month in Connection.ProcessSA_DB.MONTH.ToList())
+                {
+                    _month.Add((int)month.ID, month.NAME);
+                }
+                return _month;
+            }
+        }
+
+
+        public static Dictionary<int, string> DayOfWeek
+        {
+            get
+            {
+                _dayOfWeek = new Dictionary<int, string>();
+                foreach (Data.DAY_OF_WEEK dayOfWeek in Connection.ProcessSA_DB.DAY_OF_WEEK.ToList())
+                {
+                    _dayOfWeek.Add((int)dayOfWeek.ID, dayOfWeek.NAME);
+                }
+                return _dayOfWeek;
+            }
+        }
+
+
+        public static Dictionary<int, string> Region
+        {
+            get
+            {
+                _region = new Dictionary<int, string>();
+                foreach (Data.REGION region in Connection.ProcessSA_DB.REGION.ToList())
+                {
+                    _region.Add((int)region.ID, region.NAME);
+                }
+                return _region;
+            }
+        }
 
         public static Dictionary<int, string> Gender
         {
@@ -19,5 +78,31 @@ namespace TASKWebApp.Business.Helpers
                 return _gender;
             }
         }
+
+        public static Dictionary<int, string> GetProvicesByRegionId (int idRegion)
+        {
+            Dictionary<int, string> provinceDictionary = new Dictionary<int, string>();
+            foreach (Data.PROVINCE province in Connection.ProcessSA_DB.PROVINCE.ToList().Where(x => x.ID_REGION == idRegion))
+            {
+                provinceDictionary.Add((int)province.ID, province.NAME);
+            }
+            return provinceDictionary;
+        }
+
+        public static Dictionary<int, string> GetCommuneByProvinceId(int idProvince)
+        {
+            Dictionary<int, string> communeDictionary = new Dictionary<int, string>();
+            foreach (Data.COMMUNE commune in Connection.ProcessSA_DB.COMMUNE.ToList().Where(x => x.ID_PROVINCE == idProvince))
+            {
+                communeDictionary.Add((int)commune.ID, commune.NAME);
+            }
+            return communeDictionary;
+        }
+
+        /*
+         * ToDo:
+         * Agregar busqueda de tareas asignadas
+         * Agregar busqueda de tareas asignadas por mi.
+         */
     }
 }
---
+++ Desarrollo Aplicación Web/TASKWebApp/TASKWebApp.Testing/TestComboBoxDataLoader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TASKWebApp.Testing
+{
+    [TestClass]
+    public class TestComboBoxDataLoader
+    {
+        [TestMethod]
+        public void GenderCountIsHigherThan0()
+        {
+            Assert.IsTrue(Business.Helpers.ComboBoxDataLoader.Gender.Values.Count > 0);
+        }
+
+        [TestMethod]
+        public void RegionCountIsHigherThan0()
+        {
+            Assert.IsTrue(Business.Helpers.ComboBoxDataLoader.Region.Values.Count > 0);
+        }
+
+        [TestMethod]
+        public void DayOfWeekCountIsEquals7()
+        {
+            Assert.AreEqual(7, Business.Helpers.ComboBoxDataLoader.DayOfWeek.Values.Count);
+        }
+
+        [TestMethod]
+        public void MonthCountIsEquals12()
+        {
+            Assert.AreEqual(12, Business.Helpers.ComboBoxDataLoader.Month.Values.Count);
+        }
+
+        [TestMethod]
+        public void TaskStatusCountIsHigherThan0()
+        {
+            Assert.IsTrue(Business.Helpers.ComboBoxDataLoader.TaskStatus.Values.Count > 0);
+        }
+
+        [TestMethod]
+        public void ProvincesOfCoquimboEquals3()
+        {
+            Assert.AreEqual(3, Business.Helpers.ComboBoxDataLoader.GetProvicesByRegionId(5).Values.Count);
+        }
+
+        [TestMethod]
+        public void CommunesOfChoapaEquals4()
+        {
+            Assert.AreEqual(4, Business.Helpers.ComboBoxDataLoader.GetCommuneByProvinceId(13).Values.Count);
+        }
+
+    }
+}
