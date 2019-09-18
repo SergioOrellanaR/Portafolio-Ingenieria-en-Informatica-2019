bfe05c7e6e33ceb3c41402efccf6c4e777860543
Brayan <bra.mendez@alumnos.duoc.cl>
2019-09-18 18:02:18 -0300

    Conexion app de escritorio a base de datos oracle

--- Desarrollo Aplicación Escritorio/ProcessSAEscritorio/src/processsaescritorio/modelo/DatosConexion.java
+++ Desarrollo Aplicación Escritorio/ProcessSAEscritorio/src/processsaescritorio/modelo/DatosConexion.java
@@ -9,6 +9,9 @@ package processsaescritorio.modelo;
  *
  * @author Brayan
  */
-public class UsuarioDAO implements DatosConexion{
-    
+public interface DatosConexion {
+    public String URL     = "jdbc:oracle:thin:@localhost:1521:ORCL";
+    public String USUARIO = "ProcessSA";
+    public String CLAVE   = "pft2019";
+    public String DRIVER  = "oracle.jdbc.OracleDriver";
 }
